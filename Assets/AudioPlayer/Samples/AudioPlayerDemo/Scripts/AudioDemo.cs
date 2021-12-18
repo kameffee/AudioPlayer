5c66eb2d68c185c30247df5a9712a246d46f8dbf
kameffee <kameffee@gmail.com>
2021-12-18 16:39:14 +0900

    Revert "Rename directory"
    This reverts commit dd78901382394c3ade3f64d5dac47ad7a21e0e90.

---
+++ Assets/AudioPlayer/Samples/AudioPlayerDemo/Scripts/AudioDemo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Kameffee.AudioPlayer;
+using UnityEngine;
+
+namespace Samples.Scripts
+{
+    public class AudioDemo : MonoBehaviour
+    {
+        private void Start()
+        {
+            AudioPlayer.Instance.Initialize();
+        }
+
+        public void Play(int id)
+        {
+            AudioPlayer.Instance.Bgm.Play(id);
+        }
+
+        public void CrossFade(int id)
+        {
+            AudioPlayer.Instance.Bgm.CrossFade(id, 3f);
+        }
+
+        public void Stop()
+        {
+            AudioPlayer.Instance.Bgm.Stop();
+        }
+    }
+}
