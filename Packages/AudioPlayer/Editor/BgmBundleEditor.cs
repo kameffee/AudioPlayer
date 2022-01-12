a2ecde738c466c63f0c3336da590e00bec69c5db
kameffee <kameffee@gmail.com>
2022-01-13 01:33:51 +0900

    Change data list to ReorderableList

---
+++ Packages/AudioPlayer/Editor/BgmBundleEditor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Kameffee.AudioPlayer;
+using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+
+namespace Kameffee.AudioPlayer
+{
+    [CustomEditor(typeof(BgmBundle))]
+    public class BgmBundleEditor : Editor
+    {
+        private ReorderableList _reorderableList;
+    
+        private void OnEnable()
+        {
+            var _list = serializedObject.FindProperty("_bgmDataList");
+            _reorderableList = new ReorderableList(serializedObject, _list)
+            {
+                drawElementCallback = (rect, index, active, focused) =>
+                {
+                    rect.xMin += 10;
+                    EditorGUI.PropertyField(rect, _list.GetArrayElementAtIndex(index), true);
+                },
+                elementHeightCallback = index => EditorGUI.GetPropertyHeight(_list.GetArrayElementAtIndex(index)),
+            };
+        }
+    
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            _reorderableList.DoLayoutList();
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
